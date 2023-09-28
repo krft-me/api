c7ed7a0f8dc383b05faf750c807069ed191b37b8
brionnevi <victor.brionne@gmail.com>
2023-09-28 15:03:44 +0200

    Initial version of krftme generated by generator-jhipster@7.9.4

---
+++ src/test/java/me/krft/api/IntegrationTest.java
@@ -0,0 +1,22 @@
+package me.krft.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import me.krft.api.KrftmeApp;
+import me.krft.api.config.AsyncSyncConfiguration;
+import me.krft.api.config.EmbeddedSQL;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { KrftmeApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
